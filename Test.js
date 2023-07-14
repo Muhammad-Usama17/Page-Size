8473de5fd3a4d6870d6d9d63fa70474bee214690
Muhammad-Usama17 <85641629+Muhammad-Usama17@users.noreply.github.com>
2023-07-14 22:05:59 +0300

    Delete Test.js

--- Test.js
+++
@@ -1,8 +0,0 @@
-function size(){
-  let page = document.getElementById("page");
-  let w = getComputedStyle(page).width;
-  let h = getComputedStyle(page).height;
-  document.getElementById("demo").innerHTML = "Width = " + w + "<br>Height = " + h;
-}
-
-setInterval(size, 0);
